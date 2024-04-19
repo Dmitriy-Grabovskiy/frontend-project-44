b918698247eece607dc160dc6df6a15473c17667
Dmitriy-Grabovskiy <git@github.com>
2024-04-19 23:49:37 +0300

    fix 12

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line import/no-named-as-default, import/no-named-as-default-member
-import { codeGamesEven } from '../src/games/codeGamesEven.js';
+import codeGamesEven from '../src/games/codeGamesEven.js';
 
 codeGamesEven();
