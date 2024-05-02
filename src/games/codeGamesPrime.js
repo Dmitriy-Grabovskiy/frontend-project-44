ba63d16ff13dd6ca08dd82a42683e79a21ce7345
Dmitriy-Grabovskiy <git@github.com>
2024-05-02 17:51:15 +0300

    fix 24

--- src/games/codeGamesPrime.js
+++ src/games/codeGamesPrime.js
@@ -4,17 +4,19 @@ import { runEngine, getRandomInt } from '../index.js';
 
 function generateRound() {
     let answer;
-    let question = getRandomInt(100);
-
+    let question = getRandomInt(1);
+    if (question == 1){
+      answer = 'no';
+    }else{
     for (let i = 2; i <= question; i += 1) {
-      if ((question % i == 0 && i != question) || question == 1) {
+      if ((question % i == 0 && i != question)) {
         answer = 'no';
         break;
       } else {
         answer = 'yes';
       }
     }
-    
+  } 
  
   return [question, answer];
 }
